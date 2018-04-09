70c27399870875fd2d241068fe7d9fcde58cab05
zabulskyy <v.zabulskyy@gmail.com>
2018-04-09 15:54:29 +0300

    Activation fix

--- activation.cpp
+++ activation.cpp
@@ -2,13 +2,13 @@
 
 using Eigen::Matrix2d;
 
-virtual Matrix2d Activation::_sigmoid(Matrix2d X) {
+Matrix2d Activation::_sigmoid(Matrix2d X) {
     double sigmoid(double x) { return 1 / (1 + exp(-x)); }
     Matrix2d newX = X.unaryExpr(&sigmoid);
     return newX;
 };
 
-virtual Matrix2d Activation::_derivative_sigmoid(Matrix2d X) {
+Matrix2d Activation::_derivative_sigmoid(Matrix2d X) {
     double derivative_sigmoid(double x) { return x * (1 - x); }
     double sigmoid(double x) { return 1 / (1 + exp(-x)); }
 
@@ -17,17 +17,8 @@ virtual Matrix2d Activation::_derivative_sigmoid(Matrix2d X) {
     return newX;
 };
 
-virtual Matrix2d Activation::backward(Matrix2d chain_error) {};
+Matrix2d Activation::backward(Matrix2d chain_error) {
 
-virtual Matrix2d Activation::forward(Matrix2d X, double lr) {};
+};
 
-
-
-/*
-    def _sigmoid(self, x):
-        return 1 / (1 + np.exp(-x))
-
-    def _derivatives_sigmoid(self, x):
-        t = self._sigmoid(x)
-        return t * (1 - t)
- * */
+Matrix2d Activation::forward(Matrix2d X, double lr) {};
