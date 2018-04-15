568f2fba845dfc660abc2570bad03febf9f9a8fe
zabulskyy <v.zabulskyy@gmail.com>
2018-04-15 15:49:12 +0300

    activation fix

---
+++ CMakeFiles/test_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_test.dir/test.cpp.o"
+  "CMakeFiles/test_test.dir/neural_network.cpp.o"
+  "CMakeFiles/test_test.dir/dense.cpp.o"
+  "CMakeFiles/test_test.dir/activation.cpp.o"
+  "test_test.pdb"
+  "test_test"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
