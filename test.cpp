4902a9b962e364d501d7e8070e17b6abcafda30c
zabulskyy <v.zabulskyy@gmail.com>
2018-05-24 13:01:56 +0300

    single thread ready and tested --beta

--- test.cpp
+++ test.cpp
@@ -4,42 +4,47 @@ using Eigen::MatrixXd;
 
 int main()
 {
+
 	// * ActivationLayer Tests
-	std::cout << "ActivationLayer Test started" << std::endl;
+//	std::cout << "ActivationLayer Test started" << std::endl;
 	ActivationLayer a("sigmoid");
 
 	MatrixXd m(2, 2);
 	m << 1, 0, 5, 6;
 
+//    std::cout << "!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+//	std::cout << m.cwiseProduct(m) << std::endl;
+
 	MatrixXd test_m(2, 2);
-	test_m = a._activ_function(m);
-	std::cout << test_m << std::endl;
-	std::cout << "ActivationLayer Test ended" << std::endl;
+	test_m = a._derivative_sigmoid(m);
+//	std::cout << "m: \n" << test_m << std::endl;
+//	std::cout << test_m << std::endl;
+//	std::cout << "ActivationLayer Test ended" << std::endl;
 
 
 	// * DenseLayer Tests
-	std::cout << "DenseLayer Test started" << std::endl;
+//	std::cout << "DenseLayer Test started" << std::endl;
 	DenseLayer dl(4, 5);
-	std::cout << dl.w << std::endl;
-	std::cout << dl.config.lr << std::endl;
+//	std::cout << dl.w << std::endl;
+//	std::cout << dl.config.lr << std::endl;
 
 	MatrixXd s(2, 4);
 	s << 0.1, 0.2, 2.1, -0.5,
 		0.1, 0.2, 2.1, -0.5;
-	std::cout << s.colwise().sum() << std::endl;
-	std::cout << "DenseLayer Test ended" << std::endl;
+//	std::cout << s.colwise().sum() << std::endl;
+//	std::cout << "DenseLayer Test ended" << std::endl;
 
 
 	// Dot product Tests
-	std::cout << "Dot product Test started" << std::endl;
+//	std::cout << "Dot product Test started" << std::endl;
 	MatrixXd mat(2, 2);
 	mat << 1, 2, 2, 4;
-	std::cout << mat << std::endl;
-	std::cout << (mat * mat) << std::endl;
-	std::cout << "Dot product Test ended" << std::endl;
+//	std::cout << mat << std::endl;
+//	std::cout << (mat * mat) << std::endl;
+//	std::cout << "Dot product Test ended" << std::endl;
 
 	// Vector adding Tests
-	std::cout << "Vector adding Test started" << std::endl;
+//	std::cout << "Vector adding Test started" << std::endl;
 	MatrixXd t1(2, 4);
 	t1 << 1, 2, 3, 4, 5, 6, 7, 8;
 	
@@ -50,19 +55,19 @@ int main()
 	for(int i = 0; i < t1.rows(); i++){
 		t1.row(i) += t2;
 	}
-	std::cout << t1 << std::endl; 
-	std::cout << "Vector adding Test ended" << std::endl;
+//	std::cout << t1 << std::endl;
+//	std::cout << "Vector adding Test ended" << std::endl;
 
 
 	// NeuralNetwork Tests
 	NeuralNetworkConfig conf;
-	conf.epochs = 5000;
-	conf.lr = 0.01;
+	conf.epochs = 20000;
+	conf.lr = 0.4;
 	NeuralNetwork nn(conf);
 	nn.add(new DenseLayer(2, 6));
 	nn.add(new ActivationLayer("sigmoid"));
 	nn.add(new DenseLayer(6, 1));
-	nn.add(new ActivationLayer("sigmoid"));	
+	nn.add(new ActivationLayer("sigmoid"));
 
 
 	MatrixXd X(4, 2);
