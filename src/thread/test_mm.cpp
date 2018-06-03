15703d1834d06ef1a06597b79e6ed2758a27f28a
zabulskyy <v.zabulskyy@gmail.com>
2018-06-03 18:40:09 +0300

    refactor

---
+++ src/thread/test_mm.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+#include <thread>
+#include <Eigen/Dense>
+#include <mutex>
+#include "threads_multiplication.h"
+#include <chrono>
+
+using std::mutex;
+using std::vector;
+using std::thread;
+using std::ref;
+
+int main(){
+    Eigen::MatrixXd m1 = Eigen::MatrixXd::Ones(5000,300);
+    Eigen::MatrixXd m2 = Eigen::MatrixXd::Ones(300,5000);
+    Eigen::MatrixXd res;
+
+    for (int i = 1; i < 10; ++i) {
+        auto start_time = std::chrono::high_resolution_clock::now();
+        res = mult_with_threads(3, m1, m2);
+        auto current_time = std::chrono::high_resolution_clock::now();
+        std::cout << i << ": " << std::chrono::duration_cast<std::chrono::seconds>(current_time - start_time).count() << std::endl;
+    }
+}
